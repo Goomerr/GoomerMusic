abc40b1449e20efccbf0db39b31bacad2c373882
Goomerr <63950956+Goomerr@users.noreply.github.com>
2021-10-31 12:44:03 +0100

    proyecto acabado

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';//Programación reactiva
@@ -9,6 +9,9 @@ import { Subscription } from 'rxjs';//Programación reactiva
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
+
+  @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('')
+
 
   // mockCover: TrackModel = {
   //   cover: 'https://i.scdn.co/image/ab67616d0000b27391f7222996c531b981e7bb3d',
@@ -21,14 +24,20 @@ export class MediaPlayerComponent implements OnInit, OnDestroy {
 
   //Lista de observadores
   listObservers$: Array<Subscription> = []
+  state: string = 'paused'
 
   constructor(public multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
 
-    this.multimediaService.trackInfo$.subscribe(res => {
-      console.log('Reproduciendo esta cancion', res)
-    })
+    const observer1$ = this.multimediaService.playerStatus$
+      .subscribe(status => this.state = status)
+
+    this.listObservers$ = [observer1$]
+
+    // this.multimediaService.trackInfo$.subscribe(res => {
+    //  // console.log('Reproduciendo esta cancion', res)
+    // })
 
     // const observable1$ = this.multimediaService.myObservable1$
     //   .subscribe(
@@ -50,4 +59,13 @@ export class MediaPlayerComponent implements OnInit, OnDestroy {
     // console.log('destruido')
   }
 
+  handlePosition(event: MouseEvent): void {
+    const elNative:HTMLElement = this.progressBar.nativeElement
+    const {clientX} = event;
+    const { x, width} = elNative.getBoundingClientRect()
+    const clickX = clientX - x;
+    const percentageDFromX = (clickX * 100) / width;
+    console.log(`Click(x): ${percentageDFromX}` )
+    this.multimediaService.seekAudio(percentageDFromX)
+  }
 }
--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -14,15 +14,20 @@ export class MultimediaService {
   //myObservable1$: BehaviorSubject<any> = new BehaviorSubject('💨💨💨💨')
 
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined)
-  public audio!: HTMLAudioElement //Audio
+  public audio!: HTMLAudioElement //<audio/>
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
+  public playerStatus$: BehaviorSubject<string> = new BehaviorSubject('paused')
+  public playerPorcentage$: BehaviorSubject<number> = new BehaviorSubject(0)
 
   constructor() {
     this.audio = new Audio()
     this.trackInfo$.subscribe(responseOK => {
-     if(responseOK){
-      console.log('💥💢💢💌', responseOK)
-      this.setAudio(responseOK)
-     }
+      if (responseOK) {
+        // console.log('💥💢💢💌', responseOK)
+        this.setAudio(responseOK)
+      }
+      this.listenAllEvents()
     })
 
     //BehaviorSubject
@@ -64,21 +69,97 @@ export class MultimediaService {
   }
 
   private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false)
+    this.audio.addEventListener('timeupdate', this.calculateTime, false)
+    this.audio.addEventListener('playing', this.setPlayerStatus, false)
+    this.audio.addEventListener('playing', this.setPlayerStatus, false)
+    this.audio.addEventListener('play', this.setPlayerStatus, false)
+    this.audio.addEventListener('pause', this.setPlayerStatus, false)
+    this.audio.addEventListener('pause', this.setPlayerStatus, false)
+    this.audio.addEventListener('ended', this.setPlayerStatus, false)
+  }
+  private setPlayerStatus = (state: any) => {
+    console.log(state)
+    switch (state.type) {
+      case 'play':
+        this.playerStatus$.next('play')
+        break
+      case 'playing':
+        this.playerStatus$.next('playing')
+        break
+      case 'ended':
+        this.playerStatus$.next('ended')
+        break
+      default:
+        this.playerStatus$.next('paused')
+    }
 
   }
 
+  private calculateTime = () => {
+    // console.log('Disparando evento...-')
+    const { duration, currentTime } = this.audio
+    //console.table([duration, currentTime])
+    this.setTimeElapsed(currentTime)
+    this.setTimeRemaining(currentTime, duration)
+    this.setPorcentage(currentTime, duration)
+  }
+
+  private setPorcentage(currentTime: number, duration: number): void {
+    //duracion de la cancion 100% currentTime x
+    //porcentaje = (currentTime * 100) / duration
+    let porcentage = (currentTime * 100) / duration;
+    this.playerPorcentage$.next(porcentage)
+  }
+
+  private setTimeElapsed(currentTime: number): void {
+    let seconds = Math.floor(currentTime % 60)// obtener los segundos como numeros enteros
+    let minutes = Math.floor((currentTime / 60) % 60)// Obtener los minutos
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `${displayMinutes}:${displaySeconds}`
+    this.timeElapsed$.next(displayFormat)
+  }
+
+  private setTimeRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime;
+    let seconds = Math.floor(timeLeft % 60)// obtener los segundos como numeros enteros
+    let minutes = Math.floor((timeLeft / 60) % 60)// Obtener los minutos
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
+
+  }
   //Funciones Publicas
-  public setAudio(track: TrackModel): void{
-    console.log('💔💔🤍', track.url)
+  public setAudio(track: TrackModel): void {
+    //console.log('💔💔🤍', track.url)
     this.audio.src = track.url
-   const playPromise = this.audio.play()
+    const playPromise = this.audio.play()
     if (playPromise !== undefined) {
-      playPromise.then(function() {
+      playPromise.then(function () {
         // Automatic playback started!
-      }).catch(function(error) {
+      }).catch(function (error) {
         // Automatic playback failed.
         // Show a UI element to let the user manually start playback.
       });
     }
   }
+
+  public togglePlayer(): void {
+    (this.audio.paused) ? this.audio.play() : this.audio.pause()
+  }
+
+  public seekAudio(percentage: number): void {
+  //click porcentaje  en la barra de progreso
+    const {duration} = this.audio
+    //console.log(`Duracion: ${duration}, porcentaje ${percentage}`)
+    const percentageToSeconds = (percentage * duration) / 100;
+    //console.log(percentageToSeconds)
+    this.audio.currentTime = percentageToSeconds
+    }
+
+
 }
+
+
